977cdd5f61c0ac199714e648b1a5c6d75b9c414f
Nikita Matveev <91771676+matveevN@users.noreply.github.com>
2024-05-11 00:26:55 +0300

    Update app.go

--- app.go
+++ app.go
@@ -7,19 +7,23 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Отправляем заголовок Content-Type для HTML
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// Выводим приветственное сообщение
+	fmt.Fprintln(w, "Hello, 世界")
 
-	// Вставляем HTML-код с текстом и изображением
-	fmt.Fprintln(w, "<html><body>")
-	fmt.Fprintln(w, "<p>Hello, 世界</p>")
-	fmt.Fprintln(w, "<img src='/image.jpg' alt='Image' width='300' height='200'>")
-	fmt.Fprintln(w, "</body></html>")
+	// Выводим ASCII-арт с изображением кота
+	fmt.Fprintln(w, `
+     /\_/\  
+    / o o \ 
+  (   "   ) 
+   \~(*)~/  
+    \~Y~/   
+     |/|    
+     |_|    
+    `)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.Handle("/image.jpg", http.FileServer(http.Dir("."))) // Обрабатываем запросы к файлу изображения из текущего каталога
 	fmt.Println("Running demo app. Press Ctrl+C to exit...")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
